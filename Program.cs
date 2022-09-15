143a5a970a1d27956088a4694d414fb9e82901ac
Gundo Ntoko <gundo@purplecosmos.co.za>
2022-09-15 11:02:24 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+
+        static int countAmount(int amount)
+        {
+            int result = 0;
+
+            for (int i = 0; i <= amount; i++)
+            {
+                result += i;
+                Console.WriteLine(result);
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string greeting = "Hi there";
+            string name = "John Doe";
+            Console.WriteLine($"{greeting} {name}");
+
+            Console.WriteLine("/___|");
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+
+            int amount = countAmount(10);
+
+            Console.WriteLine(amount);
+        }
+    }
+}
